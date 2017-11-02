35440410be2253f228e4d59f05cdaa03c5ff16e5
Alex Johnson <alexmontjohn@gmail.com>
2017-11-02 06:38:24 -0700

    Add example images

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-var newDiv = document.createElement("div");
-var newContent = document.createTextNode("Hi there and greetings!");
-newDiv.appendChild(newContent); // add the text node to the newly created div.
-document.body.appendChild(newDiv);
-
 // ---- Variables ----
 const APIKEYNASA = 'pGv9lTf2ocdPsrdSrIEbZ6FCqm8cI1iACdnEJbba';
-queryMarsPhotos(APIKEYNASA, 'curiosity', 'NAVCAM', 1848, 1);
+var photoURLs = [];
 
 // ---- Functions ----
+// Queries NASA's MarsPhotos API and returns object containing metadata and image URLs
+function queryMarsPhotos(apiKey, rover, camera, sol, pages) {
+    var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover +
+        '/photos?api_key=' + apiKey +
+        '&camera=' + camera +
+        '&sol=' + sol +
+        '&page=' + pages;
+    getJSON(url, function(err, data) {
+        if (err !== null) {
+            console.log('Something went wrong: ' + err);
+        } else {
+            for (var i = 0; i < data.photos.length; i++) {
+                photoURLs.push(data.photos[i].img_src);
+                // console.log(data.photos[i].img_src);
+            }
+        }
+    });
+}
+
+// Gets JSON object from API asynchronously
 // With help from: https://stackoverflow.com/questions/12460378/how-to-get-json-from-url-in-javascript
 function getJSON(url, callback) {
     var req = new XMLHttpRequest();
@@ -24,23 +39,5 @@ function getJSON(url, callback) {
     req.send();
 }
 
-// Queries NASA's MarsPhotos API and returns object containing metadata and image URLs
-function queryMarsPhotos(apiKey, rover, camera, sol, pages) {
-    var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover +
-        '/photos?api_key=' + apiKey +
-        '&camera=' + camera +
-        '&sol=' + sol +
-        '&page=' + pages;
-    getJSON(url, function(err, data) {
-        if (err !== null) {
-            console.log('Something went wrong: ' + err);
-        } else {
-            for (var i = 0; i < data.photos.length; i++) {
-                var img = document.createElement("img");
-                img.src = data.photos[i].img_src;
-                newDiv.appendChild(img);
-                console.log(data.photos[i].img_src);
-            }
-        }
-    });
-}
+// ---- Main ----
+queryMarsPhotos(APIKEYNASA, 'curiosity', 'NAVCAM', 1848, 1);
