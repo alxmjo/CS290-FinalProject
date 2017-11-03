b8c2ad4519c92db7f4531df50fc51dfd4587acab
Alex Johnson <alexmontjohn@gmail.com>
2017-11-02 17:08:23 -0700

    Add slideshow buttons

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ---- Variables ----
 const APIKEYNASA = 'pGv9lTf2ocdPsrdSrIEbZ6FCqm8cI1iACdnEJbba';
-var photoURLs = [];
+var slideshow = document.getElementById("slideshow");
 
 // ---- Functions ----
 // Queries NASA's MarsPhotos API and returns object containing metadata and image URLs
@@ -15,8 +15,17 @@ function queryMarsPhotos(apiKey, rover, camera, sol, pages) {
             console.log('Something went wrong: ' + err);
         } else {
             for (var i = 0; i < data.photos.length; i++) {
-                photoURLs.push(data.photos[i].img_src);
-                // console.log(data.photos[i].img_src);
+                var newImage = document.createElement("img");
+                newImage.src = data.photos[i].img_src;
+                newImage.classList.add('rounded');
+                newImage.classList.add('slide');
+                var newFigure = document.createElement("figure");
+                newFigure.appendChild(newImage);
+                newFigure.classList.add('image');
+                slideshow.appendChild(newFigure);
+                console.log(data.photos[i].img_src);
+                var slideIndex = 1;
+                showDivs(slideIndex);
             }
         }
     });
@@ -39,5 +48,33 @@ function getJSON(url, callback) {
     req.send();
 }
 
+
+// With help from: https://www.w3schools.com/w3css/w3css_slideshow.asp
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+
+// With help from: https://www.w3schools.com/w3css/w3css_slideshow.asp
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    if (x[slideIndex-1] !== undefined) {
+        x[slideIndex-1].style.display = "block";
+    }
+}
+
 // ---- Main ----
 queryMarsPhotos(APIKEYNASA, 'curiosity', 'NAVCAM', 1848, 1);
+
+var slideIndex = 1;
+showDivs(slideIndex);
